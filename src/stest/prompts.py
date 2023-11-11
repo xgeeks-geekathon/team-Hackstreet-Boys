1ea99eb1235c376c78ab69bc3ef79f40f8633934
JoaoAJMatos <joaoafonsoj2004@gmail.com>
2023-11-11 19:16:48 +0000

    updates prompts for better accuracy

--- src/stest/prompts.py
+++ src/stest/prompts.py
@@ -1,30 +1,33 @@
-CONTEXT_PROMPT = """
+# This prompt is used to contextualize the task that CHAT-GPT will have to perform.
+# It is used by the create_tests command to produce tests for all the files that are
+# being tracked by stest.
+# 
+# It is the initial_prompt (check openai_iface.py/send_data_in_chunks_and_get_response)
+# which will be the task Chat GPT will have to perform after all the data is sent.
+CREATE_TESTS_PROMPT = """
     You are an experienced software developer who has been hired to
     write unit tests. You will be given a set of files and your job
     is to write unit tests for them. The files are written in {language}
     and the tests MUST be written using {test_framework}.
 
     I will now give you a set of files, where the content of each file is
-    contained between these two lines: {start_delimiter} and {end_delimiter}.
-    
-    The files will be fed in chunks using the following format:
-    <file_name>
-    <start_delimiter>
-    <file_content>
-    <end_delimiter>
+    the code that you will have to write tests for. The files will be separated
+    by the delimiter 'FILE <filename> STARTS HERE' and 'FILE <filename> ENDS HERE'. 
+    When I am finished, I will tell you 'ALL FILES SENT'.
 
-    In the end, after all files have been fed, 
-"""
+    DO NOT answer until you have received all the files.
 
-FEED_FILE_PROMPT = """
-    {file_name}
-    {start_delimiter}
-    {file_content}
-    {end_delimiter}
+    In the end, you will return the code for the tests that you have written using
+    the same delimiter '= FILE <filename> STARTS HERE =' and '= FILE <filename> ENDS HERE ='.
+
+    You MUST ONLY return the code for the tests that you have written. NOTHING ELSE.
 """
 
 # This prompt is used to check if the given file is written 
 # in the given language.
+#
+# It is the initial_prompt (check openai_iface.py/send_data_in_chunks_and_get_response)
+# which will be the task Chat GPT will have to perform after all the data is sent.
 CHECK_FILE_LANGUAGE_PROMPT = """
     I will give you a file and you will have to tell me if the file is written in {language}.
     When I am finished, I will tell you 'FILE FULLY SENT'.
@@ -38,5 +41,15 @@ SEND_FILE_PROMPT = """
     DO NOT answer until you have received all the parts.
 """
 
+SEND_FILES_PROMPT = """
+    To provide the context for the above prompt, I will send you the content of all the files in parts.
+    When I am finished, I will tell you 'ALL FILES SENT'.
+"""
+
 # This prompt informs CHAT-GPT that the file has been fully sent.
 ALL_PARTS_SENT_PROMPT = "ALL PARTS SENT"
+
+# This prompt informs CHAT-GPT that all the files have been fully sent.
+ALL_FILES_SENT_PROMPT = "ALL FILES SENT"
+
+
