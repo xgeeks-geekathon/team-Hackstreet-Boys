696233ef13d2c8f56ea0e00df1a22f3785edf17c
André Silva <2201724@my.ipleiria.pt>
2023-11-12 04:34:26 +0000

    Progress bar not working

--- gui.py
+++ gui.py
@@ -5,11 +5,11 @@ import tkinter as tk
 
 class Gradebook(tk.Frame):
     def __init__(self, master_window):
-        super().__init__(master_window)
+        super().__init__(master_window, padx=75, pady=10)
         self.grid(sticky="nsew")
 
         # Adjust background colors
-        self.configure(bg="white")
+        self.configure(bg="#263645")
         self.create_progressbar()
         self.create_file_widgets()
 
@@ -32,7 +32,7 @@ class Gradebook(tk.Frame):
             command=self.generate_tests,
             width=15,
         )
-        generate_tests_btn.grid(row=2, column=0, columnspan=2, pady=(5, 0), sticky="ew")
+        generate_tests_btn.grid(row=2, column=0, columnspan=2, pady=(15, 0), sticky="ew")
 
         # Create the "Choose Output Directory" button
         choose_output_dir_btn = ttk.Button(
@@ -41,7 +41,7 @@ class Gradebook(tk.Frame):
             command=self.choose_output_directory,
             width=20,
         )
-        choose_output_dir_btn.grid(row=3, column=0, columnspan=2, pady=(5, 0), sticky="ew")
+        choose_output_dir_btn.grid(row=3, column=0, columnspan=2, pady=(15, 0), sticky="ew")
 
         # Variables to store output directory
         self.output_directory_var = tk.StringVar(value="No directory chosen")
@@ -53,12 +53,16 @@ class Gradebook(tk.Frame):
             style="TLabel",
             anchor="w",
         )
-        output_dir_label.grid(row=4, column=0, columnspan=2, pady=(5, 10), sticky="ew")
+        output_dir_label.grid(row=4, column=0, columnspan=2, pady=(15, 10), sticky="ew")
 
         # Adjust row and column weights
         self.rowconfigure(1, weight=1)
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
+        self.table_frame1.rowconfigure(1, weight=1)
+        self.table_frame1.columnconfigure(0, weight=1)
+        self.table_frame2.rowconfigure(1, weight=1)
+        self.table_frame2.columnconfigure(0, weight=1)
 
     def create_progressbar(self):
         self.progress_var = tk.DoubleVar()
@@ -109,20 +113,33 @@ class Gradebook(tk.Frame):
             loaded_name = os.path.basename(filenames[0])
             self.table1.insert("", "end", values=(loaded_name,))
 
-    def generate_tests(self):
-        # Get items from Table 1
-        items = self.table1.get_children()
-        total_items = len(items)
-
-        # Update progress bar based on Table 1 completion
-        for i, item in enumerate(items, start=1):
-            values = self.table1.item(item, "values")
-            self.table2.insert("", "end", values=values)
-
             # Update progress bar
-            completion_percentage = (i / total_items) * 100
+            completion_percentage = 100.0  # Assuming completion is 100% when a file is added
             self.progress_var.set(completion_percentage)
             self.update_idletasks()
+
+    def generate_tests(self):
+        # Get items from Table 1
+        items_table1 = self.table1.get_children()
+        total_items_table1 = len(items_table1)
+
+        # Get existing items in Table 2
+        existing_items_table2 = set(self.table2.item(item, "values")[0] for item in self.table2.get_children())
+
+        # Update progress bar based on the ratio of Table 2 files to Table 1 files
+        for i, item in enumerate(items_table1, start=1):
+            values = self.table1.item(item, "values")[0]  # Assuming only one column in Table 1
+
+            # Check if the file already exists in Table 2
+            if values not in existing_items_table2:
+                self.table2.insert("", "end", values=values)
+                existing_items_table2.add(values)  # Update the set of existing items
+
+            # Update progress bar
+            completion_percentage = (len(existing_items_table2) / total_items_table1) * 100
+            self.progress_var.set(completion_percentage)
+            self.update_idletasks()
+
 
     def create_table_frame(self, title, data, table_frame, bg):
         table_label = ttk.Label(table_frame, text=title, font=("Helvetica", 16), background=bg)
@@ -140,14 +157,14 @@ class Gradebook(tk.Frame):
 
         for col in coldata:
             tree.heading(col["text"], text=col["text"])
-            tree.column(col["text"], width=150)  # Adjust the width as needed
+            tree.column(col["text"], width=300)  # Adjust the width as needed
 
         tree.grid(row=1, column=0, sticky="nsew")
         table_frame.columnconfigure(0, weight=1)
         table_frame.rowconfigure(1, weight=1)
         return tree
 
-    def choose_output_directory(self):
+    def choose_output_directory(self):  
         output_directory = filedialog.askdirectory(title="Choose Output Directory")
         if output_directory:
             self.output_directory_var.set(output_directory)
@@ -156,6 +173,6 @@ class Gradebook(tk.Frame):
 if __name__ == "__main__":
     app = tk.Tk()
     app.title("Gradebook")
-    app.geometry("800x600")  # Set an initial window size
+    app.geometry("800x475")  # Set an initial window size
     Gradebook(app)
     app.mainloop()
